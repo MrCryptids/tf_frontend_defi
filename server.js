7ada2bcaae38492177356542003a037d5d180830
Mr. Cryptids <mr.cryptids@protonmail.com>
2024-08-12 21:48:40 -0400

    Grid UI foundation for Viewing all Freezers. Still need to create focused Freezer view

--- server.js
+++ server.js
@@ -11,8 +11,28 @@ app.get('/pages/:page', (req, res) => {
     res.render(`pages/${req.params.page}`);
 });
 
+
 app.get('/', (req, res) => {
-    res.render('index', { iframeSrc: 'https://curve.fi/#/ethereum/pools/factory-crypto-65' });});
+    const freezers = [
+        {
+          name: "Freezer 1",
+          imageUrl: "./images/chartplaceholder.png",
+          lockDate: "2023-01-01",
+          unlockDate: "2026-01-01",
+          wethLocked: 2
+        },
+        {
+            name: "Freezer 2",
+            imageUrl: "./images/chartplaceholder.png",
+            lockDate: "2023-05-23",
+            unlockDate: "2024-01-23",
+            wethLocked: 0.8453
+          }
+        // Add more dummy freezer objects as needed
+      ];
+
+
+    res.render('index', { iframeSrc: 'https://curve.fi/#/ethereum/pools/factory-crypto-65', freezers: freezers });});
 
 const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
